0f950cde0e6bcfaf9f8419c53cf1c2e88c8f0a8f
Manav <manav18gadhiya@gmail.com>
2025-05-30 16:33:06 +0530

    rm: upload service

--- upload-service/src/constants.ts
+++
@@ -1,6 +0,0 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-
-export const __dirname = dirname(fileURLToPath(import.meta.url));
-
-export const GITHUB_URL = "https://github.com";
