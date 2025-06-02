2859eb8cf2da0023b3820bc5c5b7126696c87881
Manav <manav18gadhiya@gmail.com>
2025-06-02 22:17:49 +0530

    edge config middleware for maintenance

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,10 +1,27 @@
 import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+import { get } from "@vercel/edge-config";
 
-export default withAuth({
-  pages: {
-    signIn: "/",
+export default withAuth(
+  async function middleware(req) {
+    const isDisabled = await get("disabled");
+    if (isDisabled === true) {
+      return new NextResponse(
+        "🚧 The app is temporarily down for maintenance.",
+        {
+          status: 503,
+        }
+      );
+    }
+
+    return;
   },
-});
+  {
+    pages: {
+      signIn: "/",
+    },
+  }
+);
 
 export const config = {
   matcher: ["/new/:path*", "/dashboard/:path*"],
